680c56606f63e2bd4b2f0ee52354d5f434cedaaa
JairH <jair.jimenez8050@alumnos.udg.mx>
2025-08-20 11:59:11 -0600

    Ya quedó

--- Restaurante.cpp
+++ Restaurante.cpp
@@ -4,9 +4,18 @@ using namespace std;
 #include <cstdlib>
 #include <ctime>
 
+void cocinar(string accion, string cliente, string comida, int id) {
+    cout << "Esclavo " << id << ": " << accion << " " << comida
+         << " para " << cliente << endl;
+}
 
-void pedido () 
+void pedido (int contador) 
 {
+    if (contador == 4)
+    {
+       return;
+    }
+    
     string menu[] = {"Pizza", "Sopes", "Enchiladas", "Waffles"};
     string Acciones[] = {"Cortar", "Picar", "Sazonar", "Hornear", "Juntar"};
     string clientes[] = {"Luis", "Alexis", "Chuyito", "Efra", "Omar"};
@@ -21,13 +30,30 @@ void pedido ()
     cout<<"El cliente " << Cliente_al << " Pidio "<< Comida_al<<endl;
 
 
+    thread t1(cocinar, Acciones[0], Cliente_al, Comida_al, 1);
+    thread t2(cocinar, Acciones[1], Cliente_al, Comida_al, 2);
+    thread t3(cocinar, Acciones[2], Cliente_al, Comida_al, 3);
+    thread t4(cocinar, Acciones[3], Cliente_al, Comida_al, 4);
+    thread t5(cocinar, Acciones[4], Cliente_al, Comida_al, 5);
+
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+    t5.join();
+
+    cout<<"Sale pedido de " << Cliente_al << endl;
+
+    pedido (contador+1);
+
     // string Acciones_al = Acciones[random];
 
 }
 
 int main ()
 {  
+    int i = 0;
     srand(time(0)); 
-    pedido();
+    pedido(i);
     
 }
