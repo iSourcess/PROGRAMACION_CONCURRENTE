22aac2b227a83373b9d3fa0602356d8339677260
JairH <jair.jimenez8050@alumnos.udg.mx>
2025-08-20 11:31:08 -0600

    Ya pide la gente

--- Restaurante.cpp
+++ Restaurante.cpp
@@ -4,17 +4,30 @@ using namespace std;
 #include <cstdlib>
 #include <ctime>
 
-main(restaurante)
+
+void pedido () 
 {
-    menu = {Pizza, Sopes, Enchiladas, Waffles};
-    Acciones = {Cortar, Picar, Sazonar, Hornear, Juntar};
-    clientes = {Luis, Alexis, Chuyito, Efra, Omar};
+    string menu[] = {"Pizza", "Sopes", "Enchiladas", "Waffles"};
+    string Acciones[] = {"Cortar", "Picar", "Sazonar", "Hornear", "Juntar"};
+    string clientes[] = {"Luis", "Alexis", "Chuyito", "Efra", "Omar"};
 
-    Cliente_al = rand(clientes);
-    cout<<Cliente_al;
+
+    int random1 = rand() % 5; 
+    int random2 = rand() % 4;
+    string Cliente_al = clientes[random1];
+    string Comida_al = menu[random2];
+ 
+
+    cout<<"El cliente " << Cliente_al << " Pidio "<< Comida_al<<endl;
+
+
+    // string Acciones_al = Acciones[random];
+
+}
+
+int main ()
+{  
+    srand(time(0)); 
+    pedido();
     
-    // for(int i=0; i<3; i++){
-    //     cout<<"El cliente "
-    // }
-
 }
